7cb949eefa0cdd1d872afa5f54895e9dabc3a3ca
Alex <alexandertuchler@gmail.com>
2018-09-22 06:13:34 -0400

    Make login system fully operational and begin to add functionality to display user profile info on main page

--- __init__.py
+++ __init__.py
@@ -7,12 +7,13 @@ from front_end import frontend
 from nav import nav
 from login_data import login_data
 from forms import SignupForm
+from forms import LoginForm
 from config import Config
 
 import json
 
-from receipt.data import User
-from receipt.receipt_reader import read_receipt
+##from receipt.data import User
+##from receipt.receipt_reader import read_receipt
 
 
 app = Flask(__name__)
@@ -39,21 +40,46 @@ def create_app(configfile = None):
     return app
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    data_fields = ["phone", "last_name"]
+    form = LoginForm()
     f = open('is_logged_in.txt', 'r').read().splitlines()
     is_logged_in = True if f[0] == "True" else False
-    print(login_data_list)
-    if is_logged_in == False:
-        set_login_state('True')
-        return render_template("login.html")
-    else:
-        is_logged_in = True
+    account_data = {"phone":"","last_name":""}
+    input_data = request.full_path[2:]
+    chars = ""
+    take_chars = False
+    index = 0
+    for c in input_data:
+        if c == '&':
+            account_data[data_fields[index]] = chars
+            index += 1
+            chars = ""
+            take_chars = False
+        if take_chars:
+            chars += c
+        if c == '=':
+            take_chars = True
+    account_data[data_fields[index]] = chars
+    compare_data = None
+    
+    with open("account_data.json") as data:
+        compare_data = json.load(data)
+    
+    if compare_data[data_fields[1]] == account_data[data_fields[1]] and compare_data[data_fields[0]] == account_data[data_fields[0]]:
+        set_login_state("True")
         return redirect("home")
+    else:
+        return render_template("login.html", title = 'Signup', form=form)
 
 
 @app.route('/home')
 def main_page():
+    user_data = None
+    with open('account_data.json') as data:
+        user_data = json.load(data)
+    full_name = user_data['first_name'] + " " + user_data['last_name']
     return render_template('home.html')
 
 
