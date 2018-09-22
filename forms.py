460753646ab4372678e9f171dbdf5650930ebd2b
Alex <alexandertuchler@gmail.com>
2018-09-22 01:50:52 -0400

    Implementing functionality of forms.py

--- forms.py
+++ forms.py
@@ -6,9 +6,8 @@ class SignupForm(Form):
     first_name = TextField(u'First Name', validators=[Required()])
     last_name = TextField(u'Last Name', validators=[Required()])
     email = TextField(u'Email Address', validators=[Email()])
-    birthday = DateField(u'Your Birthday')
     phone_number = TextField(u'Cell Phone Number', validators=[Required()])
     income = IntegerField(u'Income', validators=[Required()])
-    donated_before = BooleanField(u'Have you donated to Goodwill before?', validators=[Required()])
+    occupation = TextField(u'Ocupation', validators=[Required()])
 
     submit = SubmitField(u'Submit')
