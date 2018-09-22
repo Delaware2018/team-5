7d3544f92406d0a5419d074b9f15cfd563bca428
John Hsu <johnhsu@Johns-MacBook-Pro.local>
2018-09-22 00:55:50 -0400

    Added __init__.py file

---
+++ __init__.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_appconfig import AppConfig
+
+from front_end import frontend
+from nav import nav
+from login_data import login_data
+
+
+app = Flask(__name__)
+login_data_list = list()
+
+with open('\static\database.json', encoding = 'utf-8') as data:
+    login_data_list = json.loads(data.read())
+
+
+def create_app(configfile = None):
+    #app = Flask(__name__)
+    AppConfig(app)
+    Bootstrap(app)
+    app.register_blueprint(frontend)
+    app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+    nav.init_app(app)
+    return app
+
+
+@app.route('/login/')
+def index():
+<<<<<<< HEAD
+    return render_template("home.html")
+=======
+    return render_template("main.html")
+
+@app.route('
+>>>>>>> a749b9e23a3f48574ff39bfcd9847bb133e5d3a1
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
