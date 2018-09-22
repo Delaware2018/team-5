3d616479024940c45db462924738e8cdd3b48323
Alex <alexandertuchler@gmail.com>
2018-09-22 09:08:49 -0400

    Add comments to forms.py

--- forms.py
+++ forms.py
@@ -2,6 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms.fields import *
 from wtforms.validators import Required, Email
 
+#The SignupForm class is used in __init__.py for the signup web page
+#as the inputs here are dictated by certain type fields and whether
+#they are required or not
+
 class SignupForm(FlaskForm):
     firstNameInput = TextField(u'First Name', validators=[Required()], render_kw={"placeholder": "First Name"})
     lastNameInput = TextField(u'Last Name', validators=[Required()], render_kw={"placeholder": "Last Name"})
@@ -12,6 +16,10 @@ class SignupForm(FlaskForm):
     ageInput = IntegerField(u'Age', validators=[Required()], render_kw={"placeholder": "Age"})
 
     submit = SubmitField(u'Submit')
+
+#The LoginForm class has the same functionality as the SignupForm class however
+#it is more limited as a login requires substantially less information than
+#is required for Signup
 
 class LoginForm(FlaskForm):
     lastNameInput = TextField(u'Last Name', validators=[Required()], render_kw={"placeholder": "Last Name"})
