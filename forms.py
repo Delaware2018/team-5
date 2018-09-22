9cd5c6f22ed2e6bdb38028e0567e27617ae808c8
Fivestarhobbes <john.r.n.hsu@gmail.com>
2018-09-22 02:56:20 +0000

    Fix spelling issue in forms.py

--- forms.py
+++ forms.py
@@ -9,6 +9,6 @@ class SignupForm(Form):
     birthday = DateField(u'Your Birthday')
     phone_number = TextField(u'Cell Phone Number', validators=[Required()])
     income = IntegerField(u'Income', validators=[Required()])
-    donated_before = BooleanField(u'Have you donated to GoodWill before?', validators=[Required()])
+    donated_before = BooleanField(u'Have you donated to Goodwill before?', validators=[Required()])
 
     submit = SubmitField(u'Submit')
