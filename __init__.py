5a182e5289e99c48111c1d5e36989f45ad4d50df
William Tong <williamtong105@gmail.com>
2018-09-22 04:37:47 -0400

    bundled stuff

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash
 from flask_bootstrap import Bootstrap
 from flask_appconfig import AppConfig
 from flask import g
@@ -112,6 +112,22 @@ def signup():
         flash("Login requested for user {}, remember_me = {}", format(form.phoneNumberInput, form.firstNameInput, form.lastNameInput, form.emailInput, form.ageInput, form.occupationInput, form.incomeInput))
         return redirect('/')
     return render_template('survey.html', title = 'Signup', form=form)'''
+
+@app.route('/profile')
+def profile_page():
+    out_str = ''
+    out_str += 'Name: %s %s<br />' % (user['first_name'], user['last_name'])
+    out_str += 'History: <br />'
+    
+    for item in user['data']['history']:
+        out_str += '\t%s<br />' % item
+
+    out_str += '<br />%s<br />' % user
+
+    return out_str
+
+
+
 
 
 if __name__ == "__main__":
