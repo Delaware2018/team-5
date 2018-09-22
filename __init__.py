0fc1871b5072e080198bbcada682168e1e1f4fe0
Alex <alexandertuchler@gmail.com>
2018-09-22 00:57:18 -0400

    Add routing to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_appconfig import AppConfig
+from flask import g
 
 from front_end import frontend
 from nav import nav
 from login_data import login_data
 
+import json
+
 
 app = Flask(__name__)
-login_data_list = list()
+#global login_data_list
+#global is_logged_in
 
-with open('\static\database.json', encoding = 'utf-8') as data:
+
+with open("static/database.json") as data:
     login_data_list = json.loads(data.read())
 
 
@@ -24,13 +29,18 @@ def create_app(configfile = None):
     return app
 
 
-@app.route('/login/')
+@app.route('/')
 def index():
-    return render_template("main.html")
-
-@app.route('
+    if is_logged_in == False:
+        is_logged_in = True
+        return render_template("login.html")
+    else:
+        is_logged_in = True
+        return render_template("home.html")
 
 
 if __name__ == "__main__":
+    login_data_list = list()
+    is_logged_in = False
     app.debug = True
     app.run()
