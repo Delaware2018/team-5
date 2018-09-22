24fcba70edea22eaa079167a8ddfb7115f6ad789
Fivestarhobbes <john.r.n.hsu@gmail.com>
2018-09-22 01:38:25 +0000

    Update __init__.py and add forms.py

---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import Form
+from wtforms.fields import *
+from wtforms.validators import Required, Email
+
+class SignupForm(Form):
+    first_name = TextField(u'First Name', validators=[Required()])
+    last_name = TextField(u'Last Name', validators=[Required()])
+    email = TextField(u'Email Address', validators=[Email()])
+    birthday = DateField(u'Your Birthday')
+    phone_number = TextField(u'Cell Phone Number', validators=[Required()])
+    income = IntegerField(u'Income', validators=[Required()])
+    donated_before = BooleanField(u'Have you donated to GoodWill before?', validators=[Required()])
+
+    submit = SubmitField(u'Submit')
